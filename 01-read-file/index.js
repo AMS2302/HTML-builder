0b3b85982c24866f215229d84d644c07a471d660
AMS2302 <ams23post@gmail.com>
2022-05-19 18:11:56 +0300

    refactor: solution task 01-read-file

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -3,4 +3,9 @@ const fs = require('fs');
 const path = require('path');
 
 const stream = fs.createReadStream(path.join(__dirname, 'text.txt'), 'utf-8');
-stream.on('data', chunk => stdout.write(chunk));
+
+let data = '';
+
+stream.on('data', chunk => data += chunk);
+stream.on('end', () => stdout.write(data));
+stream.on('error', error => console.log('Error', error.message));
